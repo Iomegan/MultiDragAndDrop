b12e333bf92d7e7817ec05a648df8090bdcbdc8c
Iomegan <dwitt.kiel@icloud.com>
2023-01-02 12:08:28 +0100

    Initial Commit

---
+++ MultiDragAndDrop/MultiDragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiDragAndDropApp.swift
+//  MultiDragAndDrop
+//
+//  Created by Daniel Witt on 02.01.23.
+//
+
+import SwiftUI
+
+@main
+struct MultiDragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
